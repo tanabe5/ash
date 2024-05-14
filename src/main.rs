c4d8a0f19b1a331f120c63e8b2f8ba641d15212b
tanabe <i2486129@gmail.com>
2024-05-14 13:25:24 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::path::PathBuf;
+use clap::Parser;
+use cli::{RunMode, CliOpts};
+
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    let _opts = CliOpts::parse();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_run() {
+        let opts = CliOpts::parse_from(&["ash_test", "-o", "test.zip", "src", "LICENSE", "README.md", "Cargo.toml"]);
+        assert_eq!(opts.mode, RunMode::Auto);
+        assert_eq!(opts.output, Some(PathBuf::from("test.zip")));
+        assert_eq!(opts.args.len(), 4);
+        assert_eq!(opts.args, vec![PathBuf::from("src"), PathBuf::from("LICENSE"), PathBuf::from("README.md"), PathBuf::from("Cargo.toml")]);
+    }
 }
